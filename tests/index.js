263dc0a7fa5c48680e06d4c82d4c4d14f07a493f
Emmanuel Olawoore <101262045+DevBadAss@users.noreply.github.com>
2022-06-23 20:52:57 +0100

    Updated Tests

--- tests/index.js
+++ tests/index.js
@@ -38,11 +38,54 @@ router.addRoute("models", "#/home");
 import DOM from "../lib/dom.js";
 
 //Instantiating
-const bodyDom = new DOM("app");
+// const bodyDom = new DOM("app");
 
 //Testing
-console.log(bodyDom.selectall("first-text"));
-console.log(bodyDom.select("first-text"));
-console.log(bodyDom.getTag("DIV"));
-console.log(bodyDom.getID("second-text"));
-console.log(bodyDom.getClass("first-text"));
+// console.log(bodyDom.selectall("first-text"));
+// console.log(bodyDom.select("first-text"));
+// console.log(bodyDom.getTag("DIV"));
+// console.log(bodyDom.getID("second-text"));
+// console.log(bodyDom.getClass("first-text"));
+// console.log(bodyDom.Attr("set", { name: "name", value: "User" }, "parent-div"))
+// bodyDom.select("test-input").onchange = function() {
+//     console.log(bodyDom.Value("test-input"));
+// }
+// bodyDom.addClass("test-input", "first-input");
+// bodyDom.setStyle("parent-div", { backgroundColor: "black", padding: "20px" })
+// bodyDom.addhtml("parent-div", "<p class='current-p'>Lorem Ipsum</p>", 3);
+// bodyDom.setStyle("current-p", { backgroundColor: "white", padding: "20px" })
+// const testString = "I love ReactJS";
+// const newTestString = bodyDom.chstr(testString, "ReactJS", "AppJS");
+// console.log(newTestString);
+// console.log(bodyDom.length(newTestString));
+
+
+// Importing dragndrop.js
+import MobileDrag from "../lib/dragndrop.js";
+//Testing MobileDrag
+// MobileDrag("parent-div");
+
+// Importing FileReader
+import readFile from "../lib/filereader.js";
+
+// readFile("file-input", "buffer", result => {
+//     console.log(result);
+// });
+
+
+//Importing screen recorder
+import startRecord from "../lib/screen-recorder.js";
+
+//Testing
+// startRecord((rec) => {
+//     bodyDom.select("test-input").onclick = (e) => {
+//         e.preventDefault();
+//         rec.stop()
+//     }
+// });
+
+//Importing components
+import Create__Component from "../lib/component.js";
+import AppTab from "../components/app-tab.js";
+
+Create__Component("app-tab", AppTab);
